656168533b59f03953b4c2c8aa599c82b7aeb390
ThManhas <thmanhas@gmail.com>
2025-07-14 11:54:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const meuBotao = document.getElementById('meuBotao');
-    const mensagem = document.getElementById('mensagem');
 
-    meuBotao.addEventListener('click', function() {
-        mensagem.textContent = 'Você clicou no botão!';
-        alert('Botão clicado!');
-    });
-});
